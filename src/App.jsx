062705c7b85d4f8d61950a25eacb2f524bf0106a
Alexis <AlexisNoMain@icloud.com>
2025-03-22 11:11:41 -0400

    Creando la pagina comentarios

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { Login } from './pages/Login.jsx';
 import { Home } from './pages/Home.jsx';
 import './styles/static.css';
+import { Comentario } from './pages/Comentario.jsx';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <Route path="/" element={<h1>Hola Mundo!</h1>} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
-        <Route path='/comentarios' element={<h1>Comentarios</h1>} />
+        <Route path='/comentarios' element={<Comentario />} />
         <Route path='/asignaturas' element={<h1>Asignaturas</h1>} />
         <Route path='/maestros' element={<h1>Maestros y Facultades</h1>} />
         <Route path='/carreras' element={<h1>Carreras</h1>} />
