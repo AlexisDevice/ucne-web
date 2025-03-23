6d5325abb40c844fe14b0b862e0364561cb2e684
Alexis <AlexisNoMain@icloud.com>
2025-03-23 17:41:18 -0400

    Actualizando el componente maestro.

--- src/pages/Maestro.jsx
+++ src/pages/Maestro.jsx
@@ -1,23 +1,31 @@
-import React from "react";
 import '../styles/maestro.css';
+import logo from '../assets/logoUCNE.png';
+import { useEffect, useState } from 'react';
 
 function Maestros() {
-    const maestros = [
-        { nombre: "Gregory Suarez", carrera: "Ingeniería" },
-        { nombre: "Enel Almonte", carrera: "Ingeniería" },
-        { nombre: "Miguel Jimenez", carrera: "Ingeniería" }
-    ];
+    const [maestros, setMaestros] = useState([]);
+
+    /* obtener maestros desde la api */
+    useEffect(() => {
+        fetch('http://localhost:5062/api/maestros')
+            .then(response => response.json())
+            .then(data => setMaestros(data));
+    }, []);
 
     return (
         <div className="maestros-container">
             <div className="maestros-box">
                 <div className="maestros-header">
                     <img 
-                        src="https://imgs.search.brave.com/SjOU17rbC7c3J8zYDemaJgjFzan7mP6tGEBpID9gqj4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly91Y25l/dmlydHVhbC51Y25l/LmVkdS5kby9wbHVn/aW5maWxlLnBocC8x/L3RoZW1lX2Jvb3N0/X3VuaW9uL2xvZ29j/b21wYWN0LzMwMHgz/MDAvLTEvZXNjdWRv/JTIwdWNuZS5wbmc"
+                        src={logo}
                         alt="UCNE Logo" 
                         className="maestros-logo"
                     />
-                    <h1 className="maestros-title">Maestros de la UCNE</h1>
+                    <h1 className="maestros-title">
+                        Maestros
+                        <br />
+                         de la UCNE
+                    </h1>
                 </div>
                 <div className="maestros-list">
                     {maestros.map((maestro, index) => (
